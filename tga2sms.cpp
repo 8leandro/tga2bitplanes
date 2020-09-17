4578dc81adef5c1e0196caca6c434acf26794c0e
Leandro Calil <71204984+8leandro@users.noreply.github.com>
2020-09-17 00:58:09 -0300

    Add files via upload

--- tga2sms.cpp
+++ tga2sms.cpp
@@ -13,43 +13,52 @@ typedef uint8_t  BYTE;
 typedef uint16_t WORD;
 typedef uint32_t DWORD;
 
-#define MAX_COLORS      16
+#define MAX_COLORS              16
+#define MIN_RASTER_WIDTH        128
+#define MIN_RASTER_HEIGHT       16
+#define TILE_WIDTH              8
+#define TILE_HEIGHT             8
 
-#define HEADER_SIZE     18
+#define HEADER_SIZE             18
 // #defines for the TGA 18-byte header
 // Adapted from https://www.fileformat.info/format/tga/egff.htm
-#define ID_LENGTH_8		0x00 // 00h  Size of Image ID field
-#define COL_MAP_TYPE_8	0x01 // 01h  Color map type
-#define IMAGE_TYPE_8	0x02 // 02h  Image type code
-#define COL_M_START_16	0x03 // 03h  Color map origin
-#define COL_M_LENGTH_16	0x05 // 05h  Color map length
-#define COL_M_DEPTH_8	0x07 // 07h  Depth of color map entries
-#define X_OFFSET_16		0x08 // 08h  X origin of image
-#define Y_OFFSET_16		0x0A // 0Ah  Y origin of image
-#define WIDTH_16		0x0C // 0Ch  Width of image
-#define HEIGHT_16		0x0E // 0Eh  Height of image
-#define PIXEL_DEPTH_8	0x10 // 10h  Image pixel size
-#define IMG_DESCR_8		0x11 // 11h  Image descriptor byte
+#define ID_LENGTH_8             0x00 // 00h  Size of Image ID field
+#define COL_MAP_TYPE_8          0x01 // 01h  Color map type
+#define IMAGE_TYPE_8            0x02 // 02h  Image type code
+#define COL_M_START_16          0x03 // 03h  Color map origin
+#define COL_M_LENGTH_16         0x05 // 05h  Color map length
+#define COL_M_DEPTH_8           0x07 // 07h  Depth of color map entries
+#define X_OFFSET_16	            0x08 // 08h  X origin of image
+#define Y_OFFSET_16             0x0A // 0Ah  Y origin of image
+#define WIDTH_16                0x0C // 0Ch  Width of image
+#define HEIGHT_16               0x0E // 0Eh  Height of image
+#define PIXEL_DEPTH_8           0x10 // 10h  Image pixel size
+#define IMG_DESCR_8             0x11 // 11h  Image descriptor byte
 
 using namespace std;
 
 // Helper function that ORes LSB as is and MSB 8 bits shifted leftwards
-WORD get_word   (const BYTE *a) { return *a | ((*(a+1))<<8); }
+WORD read_word(const BYTE *a) { return *a | ((*(a+1))<<8); }
 // Shifting bits 6 times to the right is the same as dividing by 64
 // Once r, g and b are shifted and made 2 bits, they're put together
 // into a single byte, with the following format: 00BBGGRR
-BYTE get_6b     (const BYTE r, const BYTE g, const BYTE b) {
+BYTE toBGR8(const BYTE r, const BYTE g, const BYTE b) {
     return (r>>6) | ((g>>6) << 2) | ((b>>6) << 4); }
+// Changes the extension of a given file or appends if there is none
+/*string changeExtension(const string f, const string ext) {
+    if(f.empty()) return "." + ext;
+}*/
 
 class TGA_File {
-    #define COLOR_PALETTE(c) (color_palette[((c) & 0xff)]) // helper macro that gets color and ignores the 9th bit
+    #define COLOR_PALETTE(c) (color_palette[(c)] & 0xff) // helper macro that gets color and ignores the 9th bit
 	private:
         string lastErrorStr = "";
+        string file_name = "";
 		BYTE header[HEADER_SIZE]={0};
         WORD width=0;
-        WORD height=0;
+        WORD height=0, actual_height=0; // actual height = height minus upper palette row, if present
         BYTE *raster = nullptr; // this array holds the image "as is"
-        BYTE *raster_6b = nullptr; // this one holds it one byte per pixel
+        BYTE *raster_6b[4] = {nullptr}; // these ones hold as follows: original, x-mirrored, y-mirrored, both axes mirrored
         DWORD sizeOfRaster=0;
         bool skipAttributes=true; // when image has 32 bits per pixel, this is used to skip attributes/alpha byte
         bool hasPalRow=false; // the image is, by default, treated as its first row of tiles not being color palette indexes
@@ -57,7 +66,8 @@ class TGA_File {
 	public:
         ~TGA_File(void) {
             delete[] raster;
-            delete[] raster_6b;
+            delete[] raster_6b[0]; delete[] raster_6b[1];
+            delete[] raster_6b[2]; delete[] raster_6b[3];
         }
         ////////////////////////////////////////
         char *getLastError(void) { return (char *)lastErrorStr.c_str(); }
@@ -86,8 +96,8 @@ class TGA_File {
             bool present[0x100]={false};
             int sum=0;
             for(DWORD i=0;i<(width*height);i++) {
-                if(!present[raster_6b[i]]) {
-                    present[raster_6b[i]] = true;
+                if(!present[raster_6b[0][i]]) {
+                    present[raster_6b[0][i]] = true;
                     sum++;
                 }
             }
@@ -98,21 +108,41 @@ class TGA_File {
             BYTE *raster_offset = nullptr;
             BYTE *raster6b_offset = nullptr;
             if(sizeOfRaster==0) return false;
+            raster_offset = raster;
+            for(DWORD i=0;i<(width*height);i++) { // builds the color palette
+                BYTE b,g,r;
+                b = *(raster_offset++);
+                g = *(raster_offset++);
+                r = *(raster_offset++);
+                raster_offset += skipAttributes;
+                assignIndex(toBGR8(r,g,b));
+            }
             try {
-                raster_6b = new BYTE[width*height];
+                raster_6b[0] = new BYTE[width*actual_height];
+                raster_6b[1] = new BYTE[width*actual_height];
+                raster_6b[2] = new BYTE[width*actual_height];
+                raster_6b[3] = new BYTE[width*actual_height];
             } catch(...) {
                 lastErrorStr = "ERROR: memory allocation for raster failed!";
                 return false;
             }
-            raster_offset = raster;
-            raster6b_offset = raster_6b;
-            for(DWORD i=0;i<(width*height);i++) {
-                BYTE r, g, b;
+            raster_offset = raster + (hasPalRow ? (width * TILE_HEIGHT * (header[PIXEL_DEPTH_8]/8)) : 0);
+            raster6b_offset = raster_6b[0];
+            for(DWORD i=0;i<(width*actual_height);i++) {
+                BYTE b,g,r;
                 b = *(raster_offset++);
                 g = *(raster_offset++);
                 r = *(raster_offset++);
                 raster_offset += skipAttributes; // skipAttributes will be 1 if the file holds 32 bits per pixel, 0 otherwise
-                *(raster6b_offset++) = get_6b(r,g,b);
+                *(raster6b_offset++) = toBGR8(r,g,b);
+            }
+            for(DWORD y=0;y<actual_height;y++) { // stores flipped images
+                for(DWORD x=0;x<width;x++) {
+                    BYTE pixel = raster_6b[0][y*width+x];
+                    raster_6b[1][y*width+(width-1-x)] = pixel;                      // horizontal flipping
+                    raster_6b[2][(actual_height-1-y)*width+x] = pixel;              // vertical flipping
+                    raster_6b[3][(actual_height-1-y)*width+(width-1-x)] = pixel;    // both
+                }
             }
             return true;
         }
@@ -121,6 +151,7 @@ class TGA_File {
 	    bool loadFromFile(const string f, const bool hasPalRow = false) {
             FILE *fp;
             DWORD bytesRead=0;
+            file_name = f;
             this->hasPalRow = hasPalRow;
             if((fp = fopen(f.c_str(), "rb")) == NULL) {
                 lastErrorStr = "ERROR: unable to open file " + f + "!";
@@ -138,20 +169,21 @@ class TGA_File {
                 fclose(fp);
                 return false;
             }
-            width = get_word(&header[WIDTH_16]);
-            height = get_word(&header[HEIGHT_16]);
-            if(((width % 8) != 0) || ((height % 8) != 0)) {
+            width = read_word(&header[WIDTH_16]);
+            height = read_word(&header[HEIGHT_16]);
+            actual_height = height - (hasPalRow ? TILE_HEIGHT : 0);
+            if(((width % TILE_WIDTH) != 0) || ((height % TILE_HEIGHT) != 0)) {
                 lastErrorStr = "ERROR: image has either width or height non multiple of 8!";  
                 fclose(fp);
                 return false;
             }
-            if((width < 128) || (height < 16)) {
+            if((width < MIN_RASTER_WIDTH) || (height < MIN_RASTER_HEIGHT)) {
                 // the minimum height of 16 was set originally because of the first row (palette indexes)
                 lastErrorStr = "ERROR: image is too small to extract tiles from!";
                 fclose(fp);
                 return false;
             }
-            fseek(fp, header[ID_LENGTH_8]+get_word(&header[COL_M_LENGTH_16]), SEEK_CUR); // skips ID and color map, if any
+            fseek(fp, header[ID_LENGTH_8]+read_word(&header[COL_M_LENGTH_16]), SEEK_CUR); // skips ID and color map, if any
             sizeOfRaster = header[PIXEL_DEPTH_8] / 8 * width * height;
             try {
                 raster = new BYTE[sizeOfRaster];
@@ -174,6 +206,8 @@ class TGA_File {
 } tga_file;
 
 int main(int argc, char *argv[]) {
+    bool cmdPalRow=false;
+
     printf( "▄▄▄█████▓  ▄████  ▄▄▄        ██████ ███▄ ▄███▓  ██████ \n"
             "▓  ██▒ ▓▒ ██▒ ▀█▒▒████▄  2 ▒██    ▒▓██▒▀█▀ ██▒▒██    ▒ \n"
             "▒ ▓██░ ▒░▒██░▄▄▄░▒██  ▀█▄  ░ ▓██▄  ▓██    ▓██░░ ▓██▄   \n"
@@ -190,7 +224,17 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s [SOURCE TGA FILE] [OPTIONS]\n", argv[0]);
         return -1;
     }
-    if(!tga_file.loadFromFile(argv[1])) {
+    for(int i=2;i<argc;i++) {
+        if(argv[i][0] != '-') continue;
+        switch(argv[i][1]) {
+            case 'p':
+                cmdPalRow=true;
+                break;
+            default:
+                printf("WARNING: ignoring unrecognized option \"-%c\"!\n", argv[i][1]);
+        }
+    }
+    if(!tga_file.loadFromFile(argv[1], cmdPalRow)) {
         printf("%s\n", tga_file.getLastError());
         return -1;
     }
